d0097257d04752178f459524998328473fb75e9d
josmellbu <josmell.alex@gmail.com>
2023-08-26 21:32:54 -0500

    finished gifApp

--- src/helpers/getGfs.js
+++ src/helpers/getGfs.js
@@ -6,8 +6,8 @@ export const getGifs = async (category) => {
   const gifs = data.map((img) => ({
     id: img.id,
     title: img.title,
-    url: img.images.downsized_medium_url,
+    url: img.images.downsized_medium.url,
   }));
-  console.log(gifs);
+
   return gifs;
 };
